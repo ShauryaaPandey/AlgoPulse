33cbe59ef5f4c30c2860f60e6cd0e7a47a9f5f02
Shaurya Pandey <shauryapandey40@gmail.com>
2026-09-05 23:07:19 +0530

    Removed Duplicate files and folders

--- src/types/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './auth.js';
-export * from './platform.js';
-export * from './submission.js';
-export * from './problem.js';
-export * from './skill.js';
-export * from './analytics.js';
